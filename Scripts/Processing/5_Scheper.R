865d5277e1acc1efb87e11111cd662c3a6ba8c16
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.comcd>
2022-07-24 12:53:04 +0200

    Biella dataset added

--- Scripts/Processing/5_Scheper.R
+++ Scripts/Processing/5_Scheper.R
@@ -38,9 +38,38 @@ mutate(Pollinator_species = replace(Pollinator_species,
        Pollinator_species == "Halictus/Lasioglossum", "	Halictidae sp."))  
 
 #Split data into different dataframes based on survey name
-split_intdata <- split(data, data$Site_id)
+InteractionData <- split(data, data$Site_id)
 
 #Prepare flower count data ----
 flower_count = read.csv("Data/Raw_data/5_Scheper/flower_count_scheper.csv")
 
+#Prepare metadata data ----
+meta <- tibble(
+  Doi = "https://doi.org/10.1111/1365-2664.13658",
+  Dataset_description = "Dataset on bees and hoverflies sampled in 25 grassland
+  - field boundary pairs in the area around Chize, France, 2015.
+  Flowers and plant-pollinator interactions were surveyed two to six times a year
+  (for most sites 4 times), in two 150m2 transects (15 min each)
+  per habitat on each occasion. Grasslands varied in land use intensity.",
+  Taxa_recorded = "Bees and hoverflies")
+
+#Prepare authorship data ----
+authors <- data.frame(
+  Coauthor_name = c("Jeroen Scheper", "Isabelle Badenhausser", "David Kleijn"),
+  Orcid = c("0000-0002-4314-996X", "0000-0002-6919-8647", "0000-0003-2500-7164"),
+  E_mail = c("jeroen.scheper@wur.nl", "Isabelle.Badenhausser@inrae.fr", "david.kleijn@wur.nl"))
+
+#Save data ----
+#Create metadata list
+Metadata <- list(meta) 
+Authorship <- list(authors) 
+FlowerCount <- list(flower_count) 
+#Create list with all dataframes of interest
+Scheper <- list(InteractionData, FlowerCount, Metadata, Authorship)
+#Rename list elements
+names(Scheper) <- c("InteractionData", "FlowerCount","Metadata", "Authorship")
+#Save data
+#The prefix number depends on the total number of datasets
+#This is the dataset number 7
+saveRDS(Scheper, file="Data/Clean_data/7_Scheper.RData") 
 
---
+++ Scripts/Processing/6_Biella.R
@@ -0,0 +1,49 @@
+#DATASET NUMBER 8; Biella
+
+#Load libraries
+library(tidyverse)
+
+#Prepare interaction data ----
+data = read.csv("Data/Raw_data/6_Biella/interaction_data_biella.csv")
+
+#Standardize data
+data = data %>% 
+mutate(Plant_species = str_replace(Plant_species, "_", " ")) %>% 
+mutate(Pollinator_species = str_replace(Pollinator_species, "_", " ")) %>% 
+mutate(Pollinator_species = str_to_sentence(Pollinator_species))
+
+#Split data into different dataframes based on survey name
+InteractionData <- split(data, data$Site_id)
+
+#Prepare flower count data ----
+flower_count = read.csv("Data/Raw_data/6_Biella/flower_count_biella.csv")
+#Split data into different dataframes based on survey name
+flower_count <- split(flower_count, flower_count$Site_id)
+
+#Prepare metadata data ----
+meta <- tibble(
+  Doi = "https://doi.org/10.1556/168.2017.18.1.1",
+  Dataset_description = "This dataset documents 2 communities on different slopes
+  of a mountain in the Northen Apennine",
+  Taxa_recorded = "All flower visitors")
+
+#Prepare authorship data ----
+authors <- data.frame(
+  Coauthor_name = c("Paolo Biella"),
+  Orcid = c("0000-0003-2297-006X"),
+  E_mail = c("paolo.biella@unimib.it"))
+
+#Save data ----
+#Create metadata list
+Metadata <- list(meta) 
+Authorship <- list(authors) 
+FlowerCount <- list(flower_count)
+#Create list with all dataframes of interest
+Biella <- list(InteractionData, FlowerCount, Metadata, Authorship)
+#Rename list elements
+names(Biella) <- c("InteractionData", "FlowerCount","Metadata", "Authorship")
+#Save data
+#The prefix number depends on the total number of datasets
+#This is the dataset number 8
+saveRDS(Biella, file="Data/Clean_data/8_Biella.RData") 
+
