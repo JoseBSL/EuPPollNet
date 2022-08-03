14e2e876508048423261b469abb1fa9080a5a78f
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.comcd>
2022-08-03 18:33:31 +0200

    Scripts to visualize locations of the networks

--- Scripts/Read_all.R
+++ Scripts/Read_all.R
@@ -1,4 +1,5 @@
 library('gtools') #Library that allows to sort files by numerical order
+library(tidyverse)
 
 #Read all files with their paths
 files <- dir("Data/Clean_data", pattern="*.RData", full.names=T)
@@ -19,17 +20,59 @@ names(all_data) <- file_names
 
 
 
-
-#select unique cases from the list of lists
+#Select coordinates from the list of lists----
 data <- list()
 
 for (i in 1:length(file_names)) {
   
   int_list <- all_data[[file_names[i]]]$InteractionData
-  data[[i]] <- bind_rows(lapply(int_list, function(x) x %>% 
-                select(Longitude, Latitude)),  .id = 'id')
+  data[[i]] <- bind_rows(map(int_list, function(x) x %>% #map here is the same as a lapply
+                select(Longitude, Latitude)),  .id = 'Internal_id')
 }
 
 #Rename list and 
 names(data) <- file_names
-data = bind_rows(data)
+data = bind_rows(data,  .id = 'Study_id')
+data_coord <- st_as_sf(data, coords = c(3:4)) %>% 
+st_set_crs(4326) %>% 
+st_transform(3035)
+
+
+data_coord$Study_id <- factor(data_coord$Study_id, levels = file_names)
+
+
+# install.packages("sf")
+library(sf)
+# install.packages("giscoR")
+library(giscoR)
+
+# Year
+year_ref <- 2016
+
+# Get all countries and transform to the same CRS
+cntries <- gisco_get_countries(year = year_ref,
+        resolution = 03) %>%
+        st_transform(3035)
+
+set.seed(1)
+
+colors <- c("#8AD69E", "#74C5CE",  "#FDE9DE", "#45A631", "#95F7F0", "#CB53E1",
+            "#FE90BB", "#B187E1", "#F8A982", "#0075DC", 
+            "#993F00",  "#5EF1F2", "#005C31", "#74C5CE", "black", "lightblue",  "darkorange3","#8F7C00",
+            "#8F7C00","#8F7C00", "#9DCC00", "#C20088", "gold1","#FFA8BB","#426600","94FFB5","#003380",  
+            "#FF0010","#FF0010", "salmon", "seashell3","greenyellow","mediumaquamarine")
+#To generate more random colors 
+#randomcoloR::distinctColorPalette(k = length(file_names), altCol = F, runTsne = T)
+
+# Plot
+ggplot() +
+geom_sf(data = cntries, fill = "grey80", color = "black") +
+xlim(c(2200000, 7150000)) +
+ylim(c(1380000, 5500000)) +
+theme(panel.grid.major = element_line(color = gray(0.5), linetype = "dashed", 
+        size = 0.5), panel.background = element_rect(fill = "aliceblue"),
+panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+geom_sf(data = data_coord, aes(color=Study_id), size = 1.5, stroke = 0, shape = 16) +
+theme(legend.position="none") + 
+scale_colour_manual(name = "Study \n locations", values = colors) 
+
