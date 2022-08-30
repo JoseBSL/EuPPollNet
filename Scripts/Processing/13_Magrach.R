deb9f587f0b5b1f8de17544ae40851fb09a10a1a
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.comcd>
2022-08-30 10:39:03 +0200

    Magrach dataset added, lacking description

--- Scripts/Processing/13_Magrach.R
+++ Scripts/Processing/13_Magrach.R
@@ -8,7 +8,7 @@ library(tidyverse)
 data = read_csv("Data/Raw_data/13_Magrach/interaction_data_magrach.csv", col_names = T)
 
 #Split data into different dataframes based on survey name
-InteractionData <- split(InteractionData, InteractionData$Site_id)
+InteractionData <- split(data, data$Site_id)
 
 #Prepare flower count data ----
 flower_count = read_csv("Data/Raw_data/13_Magrach/flower_count_magrach.csv", col_names = T)
@@ -19,7 +19,7 @@ FlowerCount <- split(flower_count, flower_count$Site_id)
 #Prepare metadata data ----
 Metadata <- tibble(
   Doi = NA,
-  Dataset_description = "",
+  Dataset_description = NA,
   Taxa_recorded = "All flower visitors")
 
 #Prepare authorship data ----
@@ -28,3 +28,15 @@ Authorship <- data.frame(
   Orcid = c("0000-0003-2155-7556", "", ""),
   E_mail = c("ainhoa.magrach@bc3research.org", "maddiart19@gmail.com", 
              "pauladl@enebada.eu"))
+
+#Save data ----
+#Create list with all dataframes of interest
+Magrach <- list(InteractionData, FlowerCount, Metadata, Authorship)
+#Rename list elements
+names(Magrach) <- c("InteractionData", "FlowerCount","Metadata", "Authorship")
+#Save data
+#The prefix number depends on the total number of datasets
+#This is the dataset number 15
+saveRDS(Magrach, file="Data/Clean_data/15_Magrach.RData") 
+
+
