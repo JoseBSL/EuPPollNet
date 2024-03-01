5b2159c806608b4d31b60190226608ff26234cc2
JoseBSL <barragansljose@gmail.comcd>
2024-03-01 22:05:55 +0100

    Figure phylo and coverage of bees done!

--- Scripts/Figure2_bees.R
+++ Scripts/Figure2_bees.R
@@ -57,7 +57,7 @@ filter(!Pollinator_accepted_name == "Apis mellifera") %>%
 filter(Pollinator_family %in% bee_fam) %>% 
 mutate(Pollinator_accepted_name = 
    str_replace(Pollinator_accepted_name, 
-   "Nomiapis", "Pseudapis")) 
+   "Nomiapis", "Pseudapis"))
 #------------------------------------------------------------------#
 #Check bee coverage at European level------
 #------------------------------------------------------------------#
@@ -76,6 +76,11 @@ filter(!grepl("Cubiandrena", Pollinator_accepted_name)) %>%
 filter(!grepl("Metadioxys", Pollinator_accepted_name)) %>% 
 filter(!grepl("Ensliniana", Pollinator_accepted_name)) %>% 
 filter(!grepl("Schmiedeknechtia", Pollinator_accepted_name)) %>% 
+filter(!grepl("Stenoheriades", Pollinator_accepted_name)) %>% 
+filter(!grepl("Paradioxys", Pollinator_accepted_name)) %>% 
+filter(!grepl("Haetosmia", Pollinator_accepted_name)) %>% 
+filter(!grepl("Simpanurgus", Pollinator_accepted_name)) %>% 
+filter(!grepl("Halopanurgus", Pollinator_accepted_name)) %>% 
 dplyr::pull(Pollinator_accepted_name)
 #Bee genera
 bee_genera = master_list %>% 
@@ -90,6 +95,11 @@ filter(!Genus == "Cubiandrena") %>%
 filter(!Genus == "Metadioxys") %>% 
 filter(!Genus == "Ensliniana") %>% 
 filter(!Genus == "Schmiedeknechtia") %>% 
+filter(!Genus == "Stenoheriades") %>% 
+filter(!Genus == "Paradioxys") %>% 
+filter(!Genus == "Haetosmia") %>% 
+filter(!Genus == "Simpanurgus") %>% 
+filter(!Genus == "Halopanurgus") %>% 
 distinct() %>% 
 pull()
 
@@ -146,13 +156,10 @@ common_ancestor = getMRCA(bee.tree100, c("Halictus", "Lasioglossum"))
 bee.tree100 = bind.tip(bee.tree100, "Seladonia", edge.length=NULL, where=common_ancestor, position=0)
 #Rename Pseudapis
 bee.tree100$tip.label = str_replace(bee.tree100$tip.label, "Pseudapis", "Nomiapis")
-#Add Seladonia
-common_ancestor = getMRCA(bee.tree100, c("Panurgus", "Melitturga"))
-bee.tree100 = bind.tip(bee.tree100, "Simpanurgus", edge.length=NULL, where=common_ancestor, position=0)
 #Add Parammobatodes
 common_ancestor = getMRCA(bee.tree100, c("Pasites", "Ammobates"))
 bee.tree100 = bind.tip(bee.tree100, "Parammobatodes", edge.length=NULL, where=common_ancestor, position=0)
-#Add Parammobatodes
+#Add Chiasmognathus
 common_ancestor = getMRCA(bee.tree100, c("Pasites", "Ammobates"))
 bee.tree100 = bind.tip(bee.tree100, "Chiasmognathus", edge.length=NULL, where=common_ancestor, position=0)
 
@@ -172,37 +179,140 @@ add.cherry <- function(tree, tip, new.tips) {
 ## Adding a new sister taxon with NULL branch length
 bee.tree100 = add.cherry(bee.tree100, tip = "Nomioides", new.tips = "Ceylalictus")
 bee.tree100 = add.cherry(bee.tree100, tip = "Rophites", new.tips = "Rhophitoides")
-bee.tree100 = add.cherry(bee.tree100, tip = "Panurgus", new.tips = c("Halopanurgus", "Flavipanurgus"))
+bee.tree100 = add.cherry(bee.tree100, tip = "Panurgus", new.tips = "Flavipanurgus")
 bee.tree100 = add.cherry(bee.tree100, tip = "Ancyla", new.tips = "Tarsalia")
 bee.tree100 = add.cherry(bee.tree100, tip = "Stelis", new.tips = "Ensliniana")
-bee.tree100 = add.cherry(bee.tree100, tip = "Panurgus", new.tips = c("Halopanurgus", "Flavipanurgus"))
 
 #Now prepare data for plotting
-#Create tibble with all be genera and left join the data
-#Bee genera
+#European bee spescies data
 bee_genera1 = master_list %>% 
 select("Order", "Family", "Subfamily", "Tribe", 
   "Genus", "Subgenus", "Species", "GenusAndSpecies", "TaxonomicAuthority") %>% 
 rename(Pollinator_family = Family,
        Pollinator_accepted_name = GenusAndSpecies) %>% 
-select(Pollinator_family, Genus)
+select(Pollinator_family, Genus, Pollinator_accepted_name) %>% 
 filter(Pollinator_family %in% bee_fam) %>% 
-filter(!Genus == "Tarsalia") %>% 
+#filter(!Genus == "Tarsalia") %>% 
 filter(!Genus == "Cubiandrena") %>% 
 filter(!Genus == "Metadioxys") %>% 
-filter(!Genus == "Ensliniana") %>% 
+#filter(!Genus == "Ensliniana") %>% 
 filter(!Genus == "Schmiedeknechtia") %>% 
+filter(!Genus == "Stenoheriades") %>% 
+filter(!Genus == "Paradioxys") %>% 
+filter(!Genus == "Haetosmia") %>% 
+filter(!Genus == "Simpanurgus") %>% 
+filter(!Genus == "Halopanurgus") %>% 
 mutate(Genus = str_replace(Genus, "Pseudapis", "Nomiapis")) %>% 
 group_by(Genus, Pollinator_family) %>% 
-
-distinct()
-
-bee_genera1 = tibble(label = bee_genera1)
-#Genus level
+rename(Pollinator_genus = Genus) %>% 
+summarise(Spp_number_Europe = n_distinct(Pollinator_accepted_name))
+          
+#Genus level data from Safenet
 genus_level = bees %>% 
 group_by(Pollinator_genus, Pollinator_family) %>% 
-summarise(Spp_number = n_distinct(Pollinator_accepted_name),
+summarise(Spp_number_Safenet = n_distinct(Pollinator_accepted_name),
           Interactions = log(length(Pollinator_accepted_name)+1)) %>% 
-rename(label = Pollinator_genus)
+select(!Pollinator_family)
 
-left_join(bee_genera1, genus_level)
+#Bind Europe and Safenet data
+d = left_join(bee_genera1, genus_level, by = "Pollinator_genus") %>% 
+rename(label = Pollinator_genus) 
+#mutate(Interactions = case_when(is.na(Interactions) ~ 0, T ~ Interactions))
+
+
+#Join tree with data
+tree_family_interactions <- full_join(bee.tree100, d, by="label")
+
+#Make an additional dataset to try to plot coverage with 2 bars
+d1 = d %>% 
+mutate(Percent = Spp_number_Safenet/Spp_number_Europe *100) 
+
+a= d1 %>% 
+select(label, Pollinator_family, Spp_number_Europe, Percent) %>% 
+rename(Genus1 = label, Fam = Pollinator_family, Spp = Spp_number_Europe) %>% 
+mutate(fam_group = "a")
+
+b= d1 %>% 
+mutate(Percent = 100 - Spp_number_Safenet/Spp_number_Europe *100)  %>% 
+select(label, Pollinator_family, Spp_number_Safenet, Percent) %>% 
+rename(Genus1 = label, Fam = Pollinator_family, Spp = Spp_number_Safenet) %>% 
+mutate(Percent = case_when(is.na(Percent) ~ 100, T ~ Percent))  %>% 
+mutate(fam_group = "b")
+
+
+d1 = bind_rows(a,b)
+d1$fam_group = factor(d1$fam_group, levels=c("b","a"))
+
+
+#To check family nodes
+ggtree(tree_family_interactions, size=0.1,) + 
+geom_text2(aes(subset=!isTip, label=node), hjust=-.3, size=1.75) + 
+geom_tiplab(size=2)+
+theme(plot.margin = unit(c(2,2,2,2),"cm")) +
+coord_cartesian(clip="off")
+
+
+n = tibble(node = c("131", "84", "73", "74", "111", "91"),
+           type = c("131", "84", "73", "74", "111", "91"))
+
+
+library(ggtreeExtra)
+library(ggnewscale)
+
+#Save data for plotting
+saveRDS(tree_family_interactions, "Data/Manuscript_info/bee_phylo.RData")
+saveRDS(d1, "Data/Manuscript_info/bee_data.RData")
+saveRDS(n, "Data/Manuscript_info/node_data.RData")
+
+
+#Circular layour
+ggtree(tree_family_interactions, size=0.3, open.angle=1, alpha=1)+ 
+geom_tippoint(aes(size= Interactions), fill = "tan2", color="black", shape=21) +
+geom_tiplab(linetype='dashed', linesize=.05, offset = 0.05, size=1.85, fontface=1) +
+scale_size(name = "Log(Interactions)", range = c(0,3.75))  +
+theme(legend.position = "bottom") +
+new_scale_fill() +
+geom_fruit(data = d1, geom=geom_bar,
+                    mapping=aes(y=Genus1, x=Percent, fill = fam_group),
+                    pwidth=0.1, 
+                    orientation="y", 
+                    stat="identity",
+                    color="black",
+                    offset = 0.07,
+            axis.params=list(
+                         axis = "x",
+                         text.size  = 2.5,
+                         hjust  = 1,
+                         vjust = 0.5,
+                         nbreak = 3,
+                         fontface = 2
+                     ),
+         grid.params=list()) +
+guides(fill = "none")+
+scale_fill_manual(values = c(a = "tan2", b = "lightblue3"), guide = "none")+
+geom_fruit(geom=geom_bar,
+                    mapping=aes(y=label, x=Spp_number_Europe),
+                    pwidth=0.1, 
+                    orientation="y", 
+                    stat="identity",
+                    color="black",
+                    offset = 0) +
+annotate("text", x = 13.29, y = 70, label = "SafeNet spp coverage (%)", size= 2, fontface= "bold") +
+annotate("text", x = 14.55, y = 70, label = "European species", size= 2, fontface= "bold")+
+annotate("text", x = 12.1, y = 70, label = "SafeNet interactions", size= 2, fontface= "bold")+ 
+new_scale_fill() +
+geom_hilight(data=n, aes(node=node), type = "roundrect", fill = "orange") +
+annotate("text", x = 7, y = 3.3, label = "Melittidae", size= 3)+
+annotate("text", x = 7, y = 6.5, label = "Andrenidae", size= 3)+
+annotate("text", x = 9, y = 12.5, label = "Colletidae", size= 3)+
+annotate("text", x = 6, y = 16.5, label = "Halictidae", size= 3)+
+annotate("text", x = 3, y = 28, label = "Apidae", size= 3)+
+annotate("text", x = 3.5, y = 55, label = "Megachilidae", size= 3)
+
+
+
+
+
+
+
+
